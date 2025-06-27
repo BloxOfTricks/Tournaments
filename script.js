d38ca8743332b7e4b16d0fca935d83f3f08b9112
BloxOfTricks <bloxoftricks@gmail.com>
2025-06-27 21:20:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+  // Set title
+  document.title = "AIS Tournaments";
+
+  // Set favicon
+  const link = document.createElement("link");
+  link.rel = "icon";
+  link.type = "image/png";
+  link.href = "https://i.imgur.com/Ao8yBPd.png";
+  document.head.appendChild(link);
+</script>
